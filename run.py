38f7ae6ca7caf6ba7e9974b95f3e076bc1f8b182
XEDGit <muzio200@gmail.com>
2025-10-23 00:25:32 +0200

    Parallelized with ProcPoolExec, stored and plotted best weights estimations

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import csv
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
+# Helpers and functions
 TRANSLATE = lambda x: x / 1_000_000 if x > 1 else int(x * 1_000_000)
 regress = lambda t0, t1, x: t1 * x + t0
 err = lambda x, y, t0, t1: regress(t0, t1, x) - y
@@ -9,70 +11,98 @@ err = lambda x, y, t0, t1: regress(t0, t1, x) - y
 
 def main():
 
-    file = open('data.csv', 'r')
-    data = [[TRANSLATE(int(d[0])), TRANSLATE(int(d[1]))] for d in list(csv.reader(file))[1:]]
+    epochs = 1_000_000
 
-    epochs = 100000
+    # Get data
+    file = open("data.csv", "r")
+    data = [
+        [TRANSLATE(int(d[0])), TRANSLATE(int(d[1]))] for d in list(csv.reader(file))[1:]
+    ]
 
-    # Gen lrs
+    # Generate learning rates (LRs) array in linear space
     n = 100
-    start = 1e-7
-    end = 1e-3
+    start = 0.095
+    end = 0.5
     step = (end - start) / (n - 1)
     lrs = [start + v * step for v in range(n)]
-    # lrs = [1]
 
-    plt.xlabel('epochs')
-    plt.ylabel('loss')
+    # Plot labels
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
 
-    smallest = [0, 0, 1000]
+    # Train with all LRs
+    with ProcessPoolExecutor(max_workers=50) as tpe:
+        result = list(
+            tpe.map(train, [i for i in range(n)], [data] * n, [epochs] * n, lrs)
+        )
 
-    last_loss = 0
+    # Extract weights and losses
+    ws = [r[0] for r in result]
+    all_losses = [r[1] for r in result]
 
+    # Find best
+    best = [0, 0, 1000]
 
-    for i, lr in enumerate(tqdm(lrs, 'lr')):
-        losses = train(data, epochs, lr)
-        plt.plot(losses, label=f'{lr:.6f}')
+    # Plot losses
+    for i, lr in enumerate(lrs):
+        losses = all_losses[i]
+        plt.plot(losses, label=f"{lr:.6f}")
+        if best[2] > losses[-1]:
+            best = [i, lr, losses[-1]]
+    plt.show()
 
-        tmp_smallest = smallest
-        if losses[-1] < smallest[2]:
-            tmp_smallest = [i, lr, losses[-1]]
+    # New plot window
+    plt.figure()
 
-        tqdm.write(f'min: {smallest}')
-        tqdm.write(f'diff w last: {losses[-1] - last_loss}')
-        tqdm.write(f'diff w best: {losses[-1] - smallest[2]}')
+    # Plot data
+    plt.scatter([d[0] for d in data], [d[1] for d in data])
 
-        smallest = tmp_smallest
+    # Pick best weights and estimate against ground truth
+    print("best: ", best)
+    w = ws[best[0]]
+    for k, p, pred in [[km, price, regress(*w, km)] for km, price in data]:
+        print(
+            f"eval: km: {TRANSLATE(k)}, price: {TRANSLATE(p)}, predicted: {TRANSLATE(pred)}"
+        )
 
-
-    plt.legend()
+    # Plot estimations in linear space against data points
+    n = 100
+    start = 1
+    end = -1
+    step = (end - start) / (n - 1)
+    x = [start + v * step for v in range(n)]
+    y = [regress(*w, i) for i in x]
+    plt.plot(x, y)
     plt.show()
 
 
-def train(data, epochs, lr):
-    losses = [.0] * epochs
-    w = (.0, .0)
+def train(index, data, epochs, lr):
+    losses = [0.0] * epochs
+    w = (0.0, 0.0)
     e = 0
     try:
         for e in range(epochs):
             s0 = [err(km, price, *w) for km, price in data]
             s1 = [err(km, price, *w) * km for km, price in data]
 
-            tmp_w = (w[0] - (1/len(data)) * sum(s0) * lr, w[1] - (1/len(data)) * sum(s1) * lr)
-            losses[e] = sum([err(km, price, *tmp_w) ** 2 for km, price in data]) / len(data)
-            if e >= 10 and losses[e] > losses[e-10]:
+            tmp_w = (
+                w[0] - (1 / len(data)) * sum(s0) * lr,
+                w[1] - (1 / len(data)) * sum(s1) * lr,
+            )
+            losses[e] = sum([err(km, price, *tmp_w) ** 2 for km, price in data]) / len(
+                data
+            )
+            if e >= 10 and losses[e] > losses[e - 10]:
                 break
             w = tmp_w
     except OverflowError:
         pass
 
-    losses = losses[:e+1]
+    losses = losses[: e + 1]
 
-    # for k, p, pred in [[km, price, regress(*w, km)] for km, price in data]:
-    #     print(f"km: {TRANSLATE(k)}, price: {TRANSLATE(p)}, predicted: {TRANSLATE(pred)}")
+    print(f"{index} {lr=:.7f} {losses[-1]=}")
+    return w, losses
 
-    tqdm.write(f'{lr=:.7f} {losses[-1]=}')
-    return losses
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
